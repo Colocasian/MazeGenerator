035b4f4c4bb16785f941457b2aa1c17fe88b4f8a
Rishvic Pushpakaran <rishvic@gmail.com>
2020-08-03 23:07:28 +0530

    cleaned up code a bit more

--- include/MazeGenerator/maze_def.hpp
+++ include/MazeGenerator/maze_def.hpp
@@ -6,11 +6,11 @@
 typedef char mbit;
 #define BSZ (8*sizeof(mbit))
 
-bool getcell(const mbit* mz, int cno);
-void setcellon(mbit* mz, int cno);
-void setcelloff(mbit* mz, int cno);
-void togcell(mbit* mz, int cno);
-void printmaze(const mbit *mz, int width, int height);
+bool getcell(const mbit* mz, std::size_t cno);
+void setcellon(mbit* mz, std::size_t cno);
+void setcelloff(mbit* mz, std::size_t cno);
+void togcell(mbit* mz, std::size_t cno);
+void printmaze(const mbit *mz, std::size_t width, std::size_t height);
 
 void kruskal_gen(mbit *mz, const std::size_t width, const std::size_t height, const unsigned int seed);
 void dfs_gen(mbit *mz, const std::size_t width, const std::size_t height, const unsigned int seed);
--- src/maze_def.cpp
+++ src/maze_def.cpp
@@ -2,33 +2,33 @@
 
 #include <MazeGenerator/maze_def.hpp>
 
-bool getcell(const mbit* mz, int cno) {
+bool getcell(const mbit* mz, std::size_t cno) {
     return (bool)(mz[cno/BSZ] & (1 << (cno % BSZ)));
 }
 
-void setcellon(mbit* mz, int cno) {
+void setcellon(mbit* mz, std::size_t cno) {
     mz[cno/BSZ] |= (1 << (cno % BSZ));
 }
 
-void setcelloff(mbit* mz, int cno) {
+void setcelloff(mbit* mz, std::size_t cno) {
     mz[cno/BSZ] &= ~(1 << (cno % BSZ));
 }
 
-void togcell(mbit* mz, int cno) {
+void togcell(mbit* mz, std::size_t cno) {
     mz[cno/BSZ] ^= (1 << (cno % BSZ));
 }
 
-void printmaze(const mbit* mz, int width, int height) {
-    int mzw = (width << 1) - 1;
-    int mzh = (height << 1) - 1;
-    for (int i = 0; i < mzw+1; i++)
+void printmaze(const mbit* mz, std::size_t width, std::size_t height) {
+    std::size_t mzw = (width << 1) - 1;
+    std::size_t mzh = (height << 1) - 1;
+    for (std::size_t i = 0; i < mzw+1; i++)
         std::cout << "██";
     std::cout << "██\n";
 
-    for (int i = 0; i < mzh; i++) {
+    for (std::size_t i = 0; i < mzh; i++) {
         std::cout << "██";
-        for (int j = 0; j < mzw; j++) {
-            int cellno = i * mzw + j;
+        for (std::size_t j = 0; j < mzw; j++) {
+            std::size_t cellno = i * mzw + j;
             if (cellno & 1)
                 std::cout << (getcell(mz, cellno >> 1)? "  ": "██");
             else
@@ -37,7 +37,7 @@ void printmaze(const mbit* mz, int width, int height) {
         std::cout << "██\n";
     }
 
-    for (int i = 0; i < mzw+1; i++)
+    for (std::size_t i = 0; i < mzw+1; i++)
         std::cout << "██";
     std::cout << "██\n";
 }
